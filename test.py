ab0e5f8cf1c4da006143f04c16338af462c405e3
jgp-13 <112409591+jgp-13@users.noreply.github.com>
2022-08-29 17:05:37 +0200

    Add files via upload
    uploaded first py

---
+++ test.py
@@ -0,0 +1,27 @@
+# Create 2 new lists height and weight
+height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create 2 numpy arrays from height and weight
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+print(type(np_height))
+
+# Calculate bmi
+bmi = np_weight / np_height ** 2
+
+# Print the result
+print(bmi)
+
+# For a boolean response
+if bmi[2] > 23:
+    print("hello")
+
+# Print only those observations above 23
+bmi_subset=bmi[bmi > 25]
+print(bmi_subset)
+print(bmi)
